2e87b99bac99436c2c88afa29766e5a5678ea0e7
Guilherme Alves Silveira <guilherme_alves_silve@hotmail.com>
2021-09-05 00:52:19 -0300

    Add correction so the actor can work without failure

---
+++ src/main/java/br/com/github/guilhermealvessilve/blockchainmining/akka/exercise3/Main.java
@@ -0,0 +1,9 @@
+package br.com.github.guilhermealvessilve.blockchainmining.akka.exercise3;
+
+public class Main {
+		
+	public static void main(String[] args) {
+		BlockChainMiner miner = new BlockChainMiner();
+		miner.mineBlocks();
+	}
+}
